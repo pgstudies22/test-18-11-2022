3767cf8997985b6208fa75142e4a09e29397c880
pgstudies22 <107188587+pgstudies22@users.noreply.github.com>
2022-11-24 10:02:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,159 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js'
+import { getFirestore, collection, doc, getDoc, addDoc, setDoc, onSnapshot } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js'
+import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3g4WRjt8GfXDRzFJgA0L6ygMCxpTQsqo",
+  authDomain: "test-18-11-2022-6f253.firebaseapp.com",
+  projectId: "test-18-11-2022-6f253",
+  storageBucket: "test-18-11-2022-6f253.appspot.com",
+  messagingSenderId: "471496356849",
+  appId: "1:471496356849:web:2d85c85ebd1fa201f2ca50"
+}
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+const collectionPhrases = collection(db, 'phrases')
+
+const addPhrase = async (e, user) => {
+  e.preventDefault()
+
+  try {
+    const addedDoc = await addDoc(collectionPhrases, {
+      movieTitle: DOMPurify.sanitize(e.target.title.value), 
+      phrase: DOMPurify.sanitize(e.target.phrase.value),
+      userId: user.uid
+    })
+
+    console.log('Document adicionado com o ID:', addedDoc.id)
+
+    e.target.reset()
+
+    const modalAddPhrase = document.querySelector('[data-modal="add-phrase"]')
+    M.Modal.getInstance(modalAddPhrase).close()
+  } catch (error) {
+    console.log('Problema na adição do document:', error)
+  }
+}
+
+const initCollapsibles = collapsibles => M.Collapsible.init(collapsibles)
+
+const login = async () => {
+  try {
+    const provider = new GoogleAuthProvider()
+    signInWithRedirect(auth, provider)
+  } catch (error) {
+    console.log('login error:', error)
+  }
+}
+
+const logout = async unsubscribe => {
+  try {
+    await signOut(auth)
+    unsubscribe()
+    console.log('usuário foi deslogado')
+  } catch (error) {
+    console.log('logout error:', error)
+  }
+}
+
+const handleAuthStateChanged = async user => {
+  try {
+    const result = await getRedirectResult(auth)
+    console.log('result:', result)
+  } catch (error) {
+    console.log('erro em getRedirectResult:', error)
+  }
+
+  const lis = [...document.querySelector('[data-js="nav-ul"]').children]
+
+  lis.forEach(li => {
+    const liShouldBeVisible = li.dataset.js.includes(user ? 'logged-in' : 'logged-out')
+    
+    if (liShouldBeVisible) {
+      li.classList.remove('hide')
+      return
+    }
+
+    li.classList.add('hide')
+  })
+  
+  const loginMessageExists = document.querySelector('[data-js="login-message"]')
+  loginMessageExists?.remove()
+
+  const formAddPhrase = document.querySelector('[data-js="add-phrase-form"]')
+  const phrasesList = document.querySelector('[data-js="phrases-list"]')
+  const buttonGoogle = document.querySelector('[data-js="button-google"]')
+  const linkLogout = document.querySelector('[data-js="logout"]')
+  const accountDetailsContainer = document.querySelector('[data-js="account-details"]')
+  const accountDetails = document.createElement('p')
+
+  if (!user) {
+    const phrasesContainer = document.querySelector('[data-js="phrases-container"]')
+    const loginMessage = document.createElement('h5')
+
+    loginMessage.textContent = 'Faça login para ver as frases'
+    loginMessage.classList.add('center-align', 'white-text')
+    loginMessage.setAttribute('data-js', 'login-message')
+    phrasesContainer.append(loginMessage)
+
+    formAddPhrase.removeEventListener('submit', addPhrase)
+    formAddPhrase.onsubmit = null
+    linkLogout.onclick = null
+    buttonGoogle.addEventListener('click', login)
+    phrasesList.innerHTML = ''
+    accountDetailsContainer.innerHTML = ''
+    return
+  }
+
+  const userDocRef = doc(db, 'users', user.uid)
+  const docSnapshot = await getDoc(userDocRef)
+
+  if (!docSnapshot.exists()) {
+    try {
+      await setDoc(userDocRef, {
+        name: DOMPurify.sanitize(user.displayName),
+        email: DOMPurify.sanitize(user.email)
+      })
+    } catch (error) {
+      console.log('Erro ao tentar registrar usuário no collection users:', error)
+    }
+  }
+
+  buttonGoogle.removeEventListener('click', login)
+  formAddPhrase.onsubmit = e => addPhrase(e, user)
+  const unsubscribe = onSnapshot(collectionPhrases, snapshot => {
+    const documentFragment = document.createDocumentFragment()
+
+    snapshot.docChanges().forEach(docChange => {
+      const liPhrase = document.createElement('li')
+      const movieTitleContainer = document.createElement('div')
+      const phraseContainer = document.createElement('div')
+      const { movieTitle, phrase } = docChange.doc.data()
+
+      movieTitleContainer.textContent = DOMPurify.sanitize(movieTitle)
+      phraseContainer.textContent = DOMPurify.sanitize(phrase)
+      movieTitleContainer.setAttribute('class', 'collapsible-header blue-grey-text text-lighten-5 blue-grey darken-4')
+      phraseContainer.setAttribute('class', 'collapsible-body blue-grey-text text-lighten-5 blue-grey darken-3')
+
+      liPhrase.append(movieTitleContainer, phraseContainer)
+      documentFragment.append(liPhrase)
+    })
+
+    phrasesList.append(documentFragment)
+  })
+  linkLogout.onclick = () => logout(unsubscribe)
+  initCollapsibles(phrasesList)
+  accountDetails.textContent = DOMPurify.sanitize(`${user.displayName} | ${user.email}`)
+  accountDetailsContainer.append(accountDetails)
+}
+
+const initModals = () => {
+  const modals = document.querySelectorAll('[data-js="modal"]')
+  M.Modal.init(modals)
+}
+
+onAuthStateChanged(auth, handleAuthStateChanged)
+
+initModals()
